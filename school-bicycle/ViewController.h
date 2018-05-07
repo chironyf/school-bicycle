73194efafdda6c2d35fcada40690edd55e895b01
chironyf <chiron.yf@gmail.com>
2018-05-07 20:57:53 +0800

    初始化

---
+++ school-bicycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  school-bicycle
+//
+//  Created by   chironyf on 2018/5/7.
+//  Copyright © 2018 chironyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ school-bicycle/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  school-bicycle
+//
+//  Created by   chironyf on 2018/5/7.
+//  Copyright © 2018 chironyf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
